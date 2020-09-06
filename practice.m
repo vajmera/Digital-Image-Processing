4687230be5bf0af8c18f831360139d40fc7e50ab
Vaibhav Ajmera <51504937+vajmera@users.noreply.github.com>
2020-09-06 12:17:59 +0530

    Update practice.m

--- practice.m
+++ practice.m
@@ -1,140 +1,165 @@
 clear all
 %Assignment -2
 %Part-1
- imdata=imread('pic.png'); %reading image as matrix
- imshow(imdata) %displayingimage
+%  imdata=imread('pic.png'); %reading image as matrix
+%  imshow(imdata) %displayingimage
+%  title('RGB Image');
 
- grey_scale=rgb2gray(imdata);
- subplot(2,1,1);
-  imshow(grey_scale)
-  subplot(2,1,2);
-  imhist(grey_scale);
-  title('Normal Grey-Scale Image');
+%   grey_scale=rgb2gray(imdata);
+%  figure
+%   imshow(grey_scale)
+%   title('Normal Grey Scale Image');
+% figure
+%   imhist(grey_scale);
+%   ylim([0 2000]);
+% 
+%   title('Histogram of Normal Grey-Scale Image');
 
 %increase brightness
-figure
-subplot(2,1,1);
-inc_bright=grey_scale+70;
+% figure
 
-imshow(inc_bright)
-subplot(2,1,2);
-imhist(inc_bright);
-title('Increased brightness');
+%  inc_bright=grey_scale+50;
+ 
+% 
+% imshow(inc_bright)
+% title('Increased Brightness Image');
+% figure
+% imhist(inc_bright);
+% ylim([0 2000]);
+% 
+% title('Histogram of Increased brightness Image');
 
 
 %decrease brightness
-figure
-subplot(2,1,1);
-dec_bright=grey_scale-80;
-imshow(dec_bright);
-subplot(2,1,2);
-imhist(dec_bright);
-title('Decreased brightness');
-
-
-%histogram stretching
-figure
-subplot(2,1,1);
-inc_contrast=1.75*grey_scale;
-imshow(inc_contrast)
-subplot(2,1,2);
-imhist(inc_contrast);
-title('Increased contrast');
-
-%histogram shrinking
-figure
-subplot(2,1,1);
-dec_contrast=.5*grey_scale;
-imshow(dec_contrast);
-subplot(2,1,2);
-imhist(dec_contrast);
-
-title('Decreased contrast');
-
-
-% histogram equalizsation
-% function myfu(a)
+% figure
+% % 
+ dec_bright=grey_scale-50;
 % 
-% r=size(grey_scale,1);
-% c=size(grey_scale,2);
-% fun=uint8(zeros(r,c));
-% num_of_pixels=r*c;
-% freq=zeros(256,1);
-% pdf=zeros(256,1);
-% cdf=zeros(256,1);
-% cum=zeros(256,1);
-% out=zeros(256,1);
+% imshow(dec_bright);
+% title('Decreased Brightness Image');
+% figure
+% imhist(dec_bright);
+%  ylim([0 2000])
+% title('Histogram of Decreased brightness Image');
+
+
+% %histogram stretching
+% figure
 % 
-% for i=1:r
-%     for j=1:c
-%         val=a(i,j); %intesntiy of pixel
-%         freq(val+1)=freq(val+1)+1;
-%         pdf(val +1)=freq(val+1)/num_of_pixels;
-%     end
-% end
-% suim=0;
-% L=255;
-% tum=0;
-% for i=1:size(pdf)
-%     
-%     tum=tum+freq(i);
-%     cum(i)=tum;
-%     cdf(i)=cum(i)/num_of_pixels;
-%     out(i)=round(cdf(i)*L);
-% end
+ inc_contrast=1.25*grey_scale;
+% imshow(inc_contrast)
+% title('Increased Contrast Image');
+% figure
+% imhist(inc_contrast);
+% ylim([0 2000])
+% title('Histogram of Increased contrast image');
+
+% %histogram shrinking
+%  figure
 % 
-% for i=1:r
-%     for j=1:c
-%         fun(i,j)=out(a(i,j)+1);
-%     end
-% end
-% imshow(fun);
+%  dec_contrast=.5*grey_scale;
+% imshow(dec_contrast);
+% title('Decreased contrast image');
+% 
+% figure
+%  
+% 
+% imhist(dec_contrast);
+%  ylim([0 2000])
+% 
+% title('Histogram of Decreased contrast image');
 % 
 % 
-% end
+% % histogram equalizsation
+% % function myfu(a)
+% % 
+% % r=size(grey_scale,1);
+% % c=size(grey_scale,2);
+% % fun=uint8(zeros(r,c));
+% % num_of_pixels=r*c;
+% % freq=zeros(256,1);
+% % pdf=zeros(256,1);
+% % cdf=zeros(256,1);
+% % cum=zeros(256,1);
+% % out=zeros(256,1);
+% % 
+% % for i=1:r
+% %     for j=1:c
+% %         val=a(i,j); %intesntiy of pixel
+% %         freq(val+1)=freq(val+1)+1;
+% %         pdf(val +1)=freq(val+1)/num_of_pixels;
+% %     end
+% % end
+% % suim=0;
+% % L=255;
+% % tum=0;
+% % for i=1:size(pdf)
+% %     
+% %     tum=tum+freq(i);
+% %     cum(i)=tum;
+% %     cdf(i)=cum(i)/num_of_pixels;
+% %     out(i)=round(cdf(i)*L);
+% % end
+% % 
+% % for i=1:r
+% %     for j=1:c
+% %         fun(i,j)=out(a(i,j)+1);
+% %     end
+% % end
+% % imshow(fun);
+% % 
+% % 
+% % end
+% 
+% figure
+% hold on
+% il=histeq(grey_scale);
+% title('Processed Grey Scale Image');
+% 
+% imshow(il);
+% figure
+% imhist(il);
+% ylim([0 4000])
+% title('Histogram of Processed Image GreyScale Image');
+% hold off
+% % 
+% figure
+% ik=histeq(dec_bright);
+% hold on
+% 
+% imshow(ik);
+% title('Processed Decreassed Brightness Image');
+% figure
+% imhist(ik);
+% ylim([0 4000])
+% title('Histogram of Processed DecreasedBrightness Image');
+% 
+% figure
+% om=histeq(inc_bright);
+% 
+% imshow(om);
+% title('Processed Increased Brightness Image');
+% figure
+% imhist(om);
+% ylim([0 4000])
+% title('Histogram of Processed IncreasedBrightness Image');
 
-figure
-hold on
-il=histeq(grey_scale);
-subplot(2,1,1);
-imshow(il);
-subplot(2,1,2);
-imhist(il);
-title('Processed Image GreyScale');
-hold off
+% figure
+% qm=histeq(dec_contrast);
+% 
+% imshow(qm);
+% title(' Processed Decreased Contrast Image');
+% figure
+% imhist(qm);
+% title('Histogram of Processed  Decreased Contrast Image');
+% ylim([0 4000])
 
-figure
-ik=histeq(dec_bright);
-hold on
-subplot(2,1,1);
-imshow(ik);
-subplot(2,1,2);
-imhist(ik);
-
-title('Processed Image DecreasedBrightness');
-
-figure
-om=histeq(inc_bright);
-subplot(2,1,1);
-imshow(om);
-subplot(2,1,2);
-imhist(om);
-title('Processed Image IncreasedBrightness');
-
-figure
-qm=histeq(dec_contrast);
-subplot(2,1,1);
-imshow(qm);
-subplot(2,1,2);
-imhist(qm);
-title('Processed Image DecreasedContrast');
-
-
-
-
-
-    
-
-
-
-
+% figure
+% qm=histeq(inc_contrast);
+% 
+% imshow(qm);
+% title(' Processed Increased Contrast Image');
+% figure
+% imhist(qm);
+% title('Histogram of Processed  Increased Contrast Image');
+% ylim([0 4000])
